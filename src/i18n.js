11f9cc635042933f4d51caf7ea4bbd20700e06e4
Sudarshan Agrawal <sudarshan.shubham@gmail.com>
2020-04-23 12:20:38 -0400

    Adding lazy loading for translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,25 +1,30 @@
 import i18n from "i18next";
 import detector from "i18next-browser-languagedetector";
+import backend from "i18next-xhr-backend";
 import { reactI18nextModule } from "react-i18next";
 
-import translationEN from "../public/locales/en/translation.json";
-import translationDE from "../public/locales/de/translation.json";
+// translations are already at
+// '../public/locales/en/translation.json'
+// which is the default for the xhr backend to load from
+// import translationEN from "../public/locales/en/translation.json";
+// import translationDE from "../public/locales/de/translation.json";
 
 // the translations
-const resources = {
-  en: {
-    translation: translationEN,
-  },
-  de: {
-    translation: translationDE,
-  },
-};
+// const resources = {
+//   en: {
+//     translation: translationEN,
+//   },
+//   de: {
+//     translation: translationDE,
+//   },
+// };
 
 i18n
   .use(detector)
+  .use(backend)
   .use(reactI18nextModule) // passes i18n down to react-i18next
   .init({
-    resources,
+    //resources,
     lng: "en",
     fallbackLng: "en", // use en if detected lng is not available
     keySeparator: false, // we do not use keys in form messages.welcome
