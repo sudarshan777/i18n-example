02f51bb07fa25934aa98381194d2a8ff60192284
Sudarshan Agrawal <sudarshan.shubham@gmail.com>
2020-04-23 12:10:46 -0400

    first message

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import i18n from "./i18n";
+// the hoc
+import { withNamespaces } from "react-i18next";
 
-function App() {
+function App({ t }) {
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => changeLanguage("de")}>de</button>
+      <button onClick={() => changeLanguage("en")}>en</button>
+      <h1>{t("Welcome to React")}</h1>
     </div>
   );
 }
 
-export default App;
+export default withNamespaces()(App);
